1e24320312d5577f6e43d26ab7aab21d09596b94
Mathieu Barnachon <mathieu.barnachon@gmail.com>
2020-05-07 14:29:42 +0200

    Try to quote differently

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 echo "Running test on Assertible"
 export SHA=`git rev-parse HEAD`
 json=$(curl -u $3 "https://assertible.com/deployments" -d'{ "service": "$2", "environment": "$3", "version": "$5", "ref" : "'"${SHA}"'", "github": true}')
-echo "::set-output name=json::$json"
+echo "::set-output name=json::'$json'"
