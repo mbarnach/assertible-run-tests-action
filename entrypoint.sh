8866c79ee4799ed53c6997124511456fed65fa45
Mathieu Barnachon <mathieu.barnachon@gmail.com>
2020-05-08 09:49:46 +0200

    Add a comment line.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,5 @@
 echo "Running test on Assertible"
 export SHA=`git rev-parse HEAD`
 curl -u $3 "https://assertible.com/deployments" -d'{ "service": "$2", "environment": "$3", "version": "$4", "ref" : "'"${SHA}"'", "github": true}'
+echo "tests running"
 echo "::set-output name=json::started"
