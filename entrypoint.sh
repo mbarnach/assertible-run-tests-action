9e621c67e777af50bd920a18e7f4e107176cd272
Mathieu Barnachon <mathieu@styleshoots.com>
2020-05-08 09:44:34 +0200

    Remove EnvironmentURL option.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,5 @@
 
 echo "Running test on Assertible"
 export SHA=`git rev-parse HEAD`
-curl -u $3 "https://assertible.com/deployments" -d'{ "service": "$2", "environment": "$3", "version": "$5", "ref" : "'"${SHA}"'", "github": true}'
+curl -u $3 "https://assertible.com/deployments" -d'{ "service": "$2", "environment": "$3", "version": "$4", "ref" : "'"${SHA}"'", "github": true}'
 echo "::set-output name=json::started"
